896059c301f9e6b3b3a7ec37b605f361423a262a
Maxim Zuyzko <maximzuyzko@gmail.com>
2021-08-23 23:13:22 +0400

    done

--- src/index.js
+++ src/index.js
@@ -37,9 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const LETTER_LENGTH = 10;
+const SYMBOL_LENGTH = 2;
+const DOT = '10';
+const DASH = '11';
+
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split('');
+    let result = [];
+
+    while (arr.length) {
+        let letterDecoded = arr.splice(LETTER_LENGTH * -1);
+        let letterMorse = '';
+
+        if (letterDecoded.join('') === "**********") {
+            result.push(' ');
+            continue;
+        }
+
+        for (i = 0; i < letterDecoded.length; i += SYMBOL_LENGTH) {
+            let symbol = letterDecoded.slice(i, i + SYMBOL_LENGTH).join('');
+            letterMorse += symbol === DOT ? '.' : symbol === DASH ? '-' : '';
+        }
+        result.push(MORSE_TABLE[letterMorse]);
+    }
+    return result.reverse().join('');
 }
+
 
 module.exports = {
     decode
